f0d9283b9d5828ea5b0094fd759992a97fd93dd7
KellyPark96 <kellypark2032@gmail.com>
2022-04-11 22:56:00 +0900

    app.js modify

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
 	state = {
 		isLoading: true,
 	};
+	getMoves = async () => {
+		const movies = await axios.get(
+			"https://yts.mx/api/v2/list_movies.json"
+		);
+	};
 	componentDidMount() {
 		// fetch data API
-		setTimeout(() => {
-			this.setState({ isLoading: false });
-		}, 3000);
+		this.getMovies();
 	}
 	// comment
 	render() {
