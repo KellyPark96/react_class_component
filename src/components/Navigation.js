5824a5281e4224aca8ede6b4e47a3de431eabdd1
KellyPark96 <kellypark2032@gmail.com>
2022-04-21 23:37:24 +0900

    add Link (edit)

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,14 +3,10 @@ import { Link } from 'react-router-dom';
 import styles from './Navigation.module.css';
 
 function Navigation() {
-	const [Loading, setLoading] = userState(true);
-
 	return (
 		<div className={styles.nav}>
-			{Loading ? null : (
 			<Link to="/">Home</Link>
 			<Link to="/about">About</Link>
-		)}
 		</div>
 	);
 }
