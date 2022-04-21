39d6c3fa1f42b72ce710bf7b2a87719ac39aa6e7
KellyPark96 <kellypark2032@gmail.com>
2022-04-21 23:29:41 +0900

    add Link

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import About from './routes/About';
+import Navigation from './components/Navigation';
 
 function App() {
 	return (
 		<Router>
+			<Navigation />
 			<Routes>
 				<Route path="/" exact={true} element={<Home />} />
 				<Route path="/about" exact={true} element={<About />} />
